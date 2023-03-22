63b47da520c09f55189af00814573297b8bf2ae2
Mohammed Salman <113366108+Sallu-Fullstack@users.noreply.github.com>
2023-03-22 09:43:22 +0530

    Update database.js

--- server/models/database.js
+++ server/models/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const uri = mongodb+srv://sallu_fullstack2:sallu_fullstack2%40mongodb@sallu-fullstack.yxk0z0w.mongodb.net/Recipes?retryWrites=true&w=majority;
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
