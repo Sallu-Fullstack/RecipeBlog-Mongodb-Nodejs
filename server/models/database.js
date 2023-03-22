072412500468abe73ebd94bc3a00c3cf4e168f8b
Mohammed Salman <113366108+Sallu-Fullstack@users.noreply.github.com>
2023-03-22 09:42:54 +0530

    Update database.js

--- server/models/database.js
+++ server/models/database.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const uri = mongodb+srv://sallu_fullstack2:sallu_fullstack2%40mongodb@sallu-fullstack.yxk0z0w.mongodb.net/Recipes?retryWrites=true&w=majority;
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
